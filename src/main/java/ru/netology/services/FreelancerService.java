6afeb6ada15eee057b71a937733a295f30ca830c
kinomagnum <kinomagnum@gmail.com>
2024-06-10 11:53:13 +0300

    CiklyPravkaFull

--- src/main/java/ru/netology/services/FreelancerService.java
+++ src/main/java/ru/netology/services/FreelancerService.java
@@ -2,14 +2,7 @@ package ru.netology.services;
 
 public class FreelancerService {
 
-    public static void main(String[] args) {
-        int income = 10000;
-        int expenses = 3000;
-        int threshold = 20000;
-        calculate(income, expenses, threshold);
-    }
-
-    public static int calculate(int income, int expenses, int threshold) {
+    public int calculate(int income, int expenses, int threshold) {
         int count = 0;
         int money = 0;
         int spentOnVacation = 0;
@@ -19,21 +12,15 @@ public class FreelancerService {
             if (money >= threshold) {
                 count++;
                 money = money - expenses;
-                money = money / 3;
-
-                System.out.print("Буду отдыхать. ");
-
+                spentOnVacation = money - (money / 3);
+                money = money - spentOnVacation;
+                System.out.println("Придётся отдыхать. Потратил -" + expenses + ", затем ещё -" + spentOnVacation);
             } else {
-
-                System.out.print("Придётся работать. " + "Заработал +" + income);
+                System.out.println("Придётся работать. Заработал +" + income + " потратил -" + expenses);
                 money = money + income - expenses;
             }
-
-
-
-            System.out.println(", потратил " + expenses);
-
         }
         return count;
     }
+
 }
